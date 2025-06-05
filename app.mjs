b7922637de30a82c8d89ccf91818a7ee0fc12663
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-06-05 23:39:18 +0500

    first commit

---
+++ app.mjs
@@ -0,0 +1,25 @@
+// javascript me UTF-16 used krta hai
+
+// let str1= "LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT!";
+// console.log(str1.toLowerCase()); // lorem ipsum dolor sit amet consectetur adipiscing elit!
+
+// let str2= "lorem ipsum dolor sit amet consectetur adipiscing elit!";
+// console.log(str2.toUpperCase()); // LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT!
+
+
+// let str3 = "Lorem ipsum dolor sit amet consectetur adipiscing elit!";
+// console.log(str3.length); // 55
+
+// let str4 = "Lorem ipsum dolor sit amet consectetur adipiscing elit!";
+// console.log(str4.slice(0, 5)); // Lorem
+// console.log(str4.slice(6, 11)); // ipsum
+// console.log(str4.slice(-5)); // elite!
+// console.log(str4.slice(-16, -12)); // adip
+
+// indexOf() string ka number return krta hai
+// indexOf() ko search ka liye used krty hai
+// firstindexOf() ko search ka liye used krty hai
+// // lastIndexOf() ko search ka liye used krty hai
+//  let str5 = "Lorem ipsum dolor sit amet consectetur adipiscing elit!";
+//  console.log(str5.indexOf("ipsum")); // 6
+//  console.log(str5.indexOf("ipsum",)); // 6
